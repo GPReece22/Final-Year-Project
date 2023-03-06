398043899a0ba32b2a3290ca433ef56f963b7ef8
Reece <u1955061@unimail.hud.ac.uk>
2023-03-06 12:15:43 +0000

    Removed unneeded stuff

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -10,7 +10,8 @@
         <i class="nc-icon nc-circle-09"></i>
         <p>My Profile</p>
       </sidebar-link>
-      <sidebar-link to="/admin/table-list">
+      <sidebar-link to="/admin/input-data
+">
         <i class="nc-icon nc-notes"></i>
         <p>Input Data</p>
       </sidebar-link>
